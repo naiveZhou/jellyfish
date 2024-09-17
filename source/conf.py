d1c9e7d0104f54c8810001276a90e6ce36729c74
zgc <zhouguoch@outlook.com>
2024-09-17 15:45:26 +0800

    0917_7

--- source/conf.py
+++ source/conf.py
@@ -31,9 +31,7 @@ release = '0.1'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
-'recommonmark',
-'sphinx_markdown_tables',
-'sphinxemoji.sphinxemoji',
+
 ]
 
 # Add any paths that contain templates here, relative to this directory.
